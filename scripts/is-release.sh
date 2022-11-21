ae1ebc864afddef847279b999952c7b8fbe21005
Mark Stacey <markjstacey@gmail.com>
2022-11-21 19:15:48 -0330

    Add 'commit-starts-with' optional parameter (#4)
    * Add 'commit-starts-with' optional parameter
    
    An optional parameter has been added to validate that the commit being
    evaluated starts with a specific string.
    
    If this optional parameter is set, we will check the subject line of
    the current commit to ensure it matches what was expected. We will
    replace the string "[version]" in this parameter with the current
    release version.
    
    The README has been updated to explain how this works.
    
    * Remove extra single quote
    
    Co-authored-by: Elliot Winkler <elliot.winkler@gmail.com>
    
    * Improve console message when format does not match expected
    
    Co-authored-by: Elliot Winkler <elliot.winkler@gmail.com>
    
    * Add quotes around bash script arguments
    
    Co-authored-by: Elliot Winkler <elliot.winkler@gmail.com>

--- scripts/is-release.sh
+++ scripts/is-release.sh
@@ -5,6 +5,7 @@ set -e
 set -o pipefail
 
 BEFORE="${1}"
+COMMIT_STARTS_WITH="${2}"
 
 if [[ -z $BEFORE ]]; then
   echo "Error: Before SHA not specified."
@@ -17,6 +18,14 @@ if [[ "$VERSION_BEFORE" == "$VERSION_AFTER" ]]; then
   echo "Notice: version unchanged. Skipping release."
   echo "IS_RELEASE=false" >> $GITHUB_OUTPUT
   exit 0
+elif [[ -n $COMMIT_STARTS_WITH ]]; then
+  EXPECTED_COMMIT_PREFIX="${COMMIT_STARTS_WITH//\[version\]/$VERSION_AFTER}"
+  COMMIT_MESSAGE="$(git log --max-count=1 --format=%s)"
+  if [[ ! $COMMIT_MESSAGE =~ ^$EXPECTED_COMMIT_PREFIX ]]; then
+    echo "Notice: commit message does not start with \"${COMMIT_STARTS_WITH}\". Skipping release."
+    echo "IS_RELEASE=false" >> $GITHUB_OUTPUT
+    exit 0
+  fi
 fi
- 
+
 echo "IS_RELEASE=true" >> $GITHUB_OUTPUT
